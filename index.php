78ff01efe36726b1606128455b19197633356bd8
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-08 23:01:49 +0900

    set htaccess

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 
 <head>
-    <?php
-    include $_SERVER['DOCUMENT_ROOT'] . '/meta/metaHeadStart.php';
-    ?>
-    <title>Tasc</title>
+  <?php
+  include $_SERVER['DOCUMENT_ROOT'] . '/meta/metaHeadStart.php';
+  ?>
+  <title>Tasc</title>
 </head>
 
 <body>
-    <h1>スケジュール管理アプリを製作中…</h1>
-    now
+  <h1>スケジュール管理アプリを製作中…</h1>
+  now
 </body>
 
 </html>
